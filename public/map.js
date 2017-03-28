26906e4678eb3c1ead4cfe9b92b04d9c32ddb37a
Shadasi <shadasithrowaway@gmail.com>
2017-03-28 11:12:18 -0500

    Added a county map

--- public/map.js
+++ public/map.js
@@ -315,6 +315,28 @@ function initMap() {
 
     //=======================================================================================================================
 
+    // FUSION TABLE
+    var layer = new google.maps.FusionTablesLayer({
+        query: {
+        select: '\'GEO_ID\'',
+        from: '1Yx076g0HFD2aeya8T0jBi_w1UuEYj3b06zaXXA'
+        },
+        styles: [{
+        polygonOptions:{
+            fillColor: '#FFFFFF',
+            fillOpacity: 0.001
+        },
+        polylineOptions:{
+            strokeOpacity: 0.05
+        }
+        }]
+    });
+    layer.setMap(map);
+
+
+
+
+
     // this section does an async get request and puts circles on the map based off data from
     // the mongodb database, right now it just has a couple cities with small circles
 
