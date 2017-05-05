283d25c6b265fd22c8a9188d683a50e1e788f37b
k8e <k8eSchaffer@gmail.com>
2017-05-04 22:53:13 -0500

    Removed frickin js/css from the views folder

--- public/scripts.js
+++ public/scripts.js
@@ -363,10 +363,21 @@ function postSitesInCounty(foundSites) {
 }
 
 /**
- * Login
+ * Login functions by Duane
  */
+
 $('.avatar').click(function(e) {
   $('.card').toggleClass('active');
   $(this).toggleClass('zmdi-close');
   $(this).toggleClass('zmdi-account');
 });
+
+function compareNewPass(){ 
+    var pass1 = document.getElementById("pw1"); 
+    var pass2 = document.getElementById("pw2"); 
+ 
+    if(pass1 == pass2) 
+        console.log("THESE ARE THE SAME"); 
+    else 
+        console.log("THESE ARE NOT THE SAME"); 
+};
--- views/pages/account.js
+++
@@ -1,9 +0,0 @@
-function compareNewPass(){
-    var pass1 = document.getElementById("pw1");
-    var pass2 = document.getElementById("pw2");
-
-    if(pass1 == pass2)
-        console.log("THESE ARE THE SAME");
-    else
-        console.log("THESE ARE NOT THE SAME");
-};
