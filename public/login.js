eb85b848e3c4d6f42e16248a209ae3a888ce4cf3
Shadasi <shadasithrowaway@gmail.com>
2017-05-05 00:09:02 -0500

    Got rid of pointless stuff
    ALSO COMMENTS :(

--- public/login.js
+++ public/login.js
@@ -1,3 +1,7 @@
+/*  login.js
+    This page has some jquery that does stuff in the login
+    page.  I don't know what though.*/
+
 $('.avatar').click(function(e) {
   $('.card').toggleClass('active');
   $(this).toggleClass('zmdi-close');
